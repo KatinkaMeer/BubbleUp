a3cc9cb520f8afd97da2bdb66f5089799837687c
patritzenfeld <patrick.ritzenfeld@uni-due.de>
2026-01-16 23:20:26 +0100

    view follows player

--- src/View.hs
+++ src/View.hs
@@ -5,15 +5,26 @@ module View (render) where
 import Graphics.Gloss (
   Picture,
   black,
+  blank,
   circleSolid,
   color,
+  pictures,
+  rectangleSolid,
   translate,
  )
 
 import Model (World (World, position))
 
 render :: World -> Picture
-render World {..} =
-  uncurry translate position
-    $ color black
-    $ circleSolid 25
+render World {position = (x, y), ..} =
+  pictures
+    $
+    -- player sprite
+    color black (circleSolid 25)
+      :
+      -- other stuff in the scene
+      map
+        (translate (-x) (-y))
+        [ translate 80 40 $ circleSolid 30,
+          translate (-250) 0 $ rectangleSolid 100 1000
+        ]
